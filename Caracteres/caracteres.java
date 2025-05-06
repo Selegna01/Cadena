0992470795ee0634190aa80a0e55086466369f34
EstradaAngeles <computer>
2025-05-06 09:17:27 -0600

    2do cambio

---
+++ Caracteres/caracteres.java
@@ -0,0 +1,11 @@
+public class Caracteres{
+    public static void main(string args[]){
+        char letra = 'J';
+        char simbolo = '@';
+        char numero = '5';
+
+        Sytem.out.println("letra =" + letra);
+        Sytem.out.println("simbolo =" + simbolo);
+        Sytem.out.println("numero =" + numero);
+    }
+}
