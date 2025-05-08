22604ccadb47e5e7c9a82ef532bca76e568849e6
Selegna01 <computer>
2025-05-08 14:06:47 -0600

    Cambio de variables

---
+++ Cadena/Cadena.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Cadena{
+    public static void main(String[]args){
+        
+        Scanner almacenar = new Scanner(System.in); //donde "Ssytem.in" es el input, parecido al printf
+        System.out.println("Escribe tu nombre");
+        String nombre = almacenar.nextLine();
+        System.out.println("Escribe tu apellido Paterno:");
+        String apellido_Paterno = almacenar.nextLine();
+        System.out.println("Escribe tu apellido Materno: ");
+        String apellido_Materno = almacenar.nextLine();
+        
+        System.out.println("Tu nombre es: " + nombre + " " + apellido_Paterno + " " + apellido_Materno); //donde nombre, es utilizado para escanear el nombre ingresado.
+    }
+}
